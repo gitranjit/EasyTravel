80e67c3ede8157f8c321829ab891159eb714e62a
varad <varadsharadkulkarni@gmail.com>
2022-04-24 14:08:59 +0530

    Second commit

--- app/src/main/java/com/example/login_register/ForgotPassword.kt
+++ app/src/main/java/com/example/login_register/ForgotPassword.kt
@@ -6,6 +6,7 @@ import android.os.Bundle
 import android.view.View
 import android.widget.Button
 import android.widget.EditText
+import android.widget.TextView
 import android.widget.Toast
 import kotlin.random.Random
 
@@ -14,22 +15,45 @@ class ForgotPassword : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_forgot_password)
 
+        val enterotp=findViewById<EditText>(R.id.etEnterOTP)
+        val changepass=findViewById<Button>(R.id.btnChangePass)
+        val enterotpbtn=findViewById<Button>(R.id.btnEnterOtp)
         val otp= Random.nextInt(1000,9999)+1;
-        val otpbtn =findViewById<Button>(R.id.btnOtp)
-        otpbtn.setOnClickListener {
-            otpbtn.visibility= View.GONE
-           val confirmpass=findViewById<EditText>(R.id.etConfirmPassword)
+        val sendotpbtn =findViewById<Button>(R.id.btnOtp)
+        sendotpbtn.setOnClickListener {
+            sendotpbtn.visibility=View.GONE
+            Toast.makeText(this,otp.toString(),Toast.LENGTH_LONG).show()
+            enterotp.visibility=View.VISIBLE
+            enterotpbtn.visibility=View.VISIBLE
+//            changepass.visibility=View.GONE
+            val mail=findViewById<EditText>(R.id.etUsername)
+            mail.visibility=View.GONE
+            val entermail=findViewById<TextView>(R.id.tvEnterMail)
+            entermail.visibility=View.GONE
+//            val changepass=findViewById<Button>(R.id.btnChangePass)
+//            changepass.visibility=View.VISIBLE
+
+
+
+
+        }
+
+        enterotpbtn.setOnClickListener {
+//
+            enterotp.visibility=View.GONE
+            enterotpbtn.visibility=View.GONE
+
+            val confirmpass=findViewById<EditText>(R.id.etConfirmPassword)
             confirmpass.visibility=View.VISIBLE
             val newpass=findViewById<EditText>(R.id.etNewPassword)
             newpass.visibility=View.VISIBLE
-            val mail=findViewById<EditText>(R.id.etUsername)
-            mail.visibility=View.GONE
-            Toast.makeText(this,otp.toString(),Toast.LENGTH_LONG).show()
-
+            val changepass=findViewById<Button>(R.id.btnChangePass)
+            changepass.visibility=View.VISIBLE
+//            Toast.makeText(this,"Password Changed",Toast.LENGTH_SHORT).show()
+//            startActivity(Intent(this,MainActivity::class.java))
         }
-        val changepass=findViewById<Button>(R.id.btnChangePass)
-        changepass.setOnClickListener {
 
+        changepass.setOnClickListener {
             Toast.makeText(this,"Password Changed",Toast.LENGTH_SHORT).show()
             startActivity(Intent(this,MainActivity::class.java))
         }
